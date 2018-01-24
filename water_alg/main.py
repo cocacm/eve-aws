de486baff71514c81b1fdc7f238623dc672f1a54
porcupal <michael.d.sanchez@gmail.com>
2018-01-24 15:12:02 -0800

    jan 25 - tables module added/ 'eve_user' table added to dynamodb

--- water_alg/main.py
+++ water_alg/main.py
@@ -1,81 +1,60 @@
-import requests
-import boto3
+import requests  # must be included in AWS deployment package
 import json
-from decimal import Decimal, getcontext
-from boto3.dynamodb.conditions import Key, Attr
+import tables  # 'tables.py' script for querying and inserting to dynamoDB
 # PEP8 naming conventions for python variables and functions is snake_case
 # PEP8 convention for python comments is to start two spaces after a statement
-# install dependencies into folders with the same name
-
-dynamodb = boto3.resource('dynamodb')  # specifies the AWS service to use
-
-PF_TABLE = dynamodb.Table('eve_pf')  # specifies plant factor table
-DATA_TABLE = dynamodb.Table('eve_sensor')  # specifies plant sensor table
-
-PLANT_TYPE = None
-DATE_TIME = None
-
-# query primary and sort keys from 'DATA_TABLE' for updating
-# requires testing with AWS !!!
 
 def water_alg(event, context):
     # water algorithm for determining watering amount
     # plot_area is omitted because it has not been implemented yet
-    PLANT_TYPE = str(
-        event['Records'][0]['dynamodb']['Keys']['plant_type']['S'])
-    DATE_TIME = str(
+    user_info = tables.get_user_info() # list with attributes from 'eve_user' table
+
+    plant_type = user_info[1]
+    date_time = str(
         event['Records'][0]['dynamodb']['Keys']['date_time']['S'])
 
     print('plant type = {}, date time = {}'.format(
-        PLANT_TYPE, DATE_TIME))  # for testing
+        plant_type, date_time))  # for testing
 
-    pf = get_pf()
-    eto = get_eto()
+    # list with the values and results of 'water_alg' calculations
+    water_alg = [
+        get_pf(plant_type),
+        get_eto(date_time)
+        ]
 
-    gal_water_reserve = eto * pf * 0.623  # * plot_area
+    plot_size = user_info[3]
+    gal_water_reserve = eto * pf * 0.623 * plot_size
     cup_water_reserve = gal_water_reserve * 16  # converted to cups
     print ('water plot {} gallons or {} cups'.format(
         gal_water_reserve, cup_water_reserve))
 
-    # then determine if watering is required and invoke watering function
-    # if waterMoisture < 3 & waterReserveForPlot > 1:
-    #     print ("the plot will be watered")
-    #     willWater = True
-    # else:
-    #     print ("the plot will not be watered")
-    #     willWater = False
-    getcontext().prec = 38
-    write_results(str(pf), str(eto), str(gal_water_reserve)
+    water_alg.append(gal_water_reserve)
+    tables.write_results(user_info, water_alg)
 
-def get_pf():
+def get_pf(plant_type):
     # query plantfactor from database
     # plant_type is used to specify the attribute from 'PF_TABLE'
     print('retrieving pf...')
-
-    # specifying what data to query from 'PF_TABLE'
-    response = PF_TABLE.query(
-        KeyConditionExpression = Key('plant_type').eq(PLANT_TYPE))
-    items = response['Items']  # query returns a JSON object, 'Items'
-
-    # assign the plant factor value from 'PF_TABLE' to a variable
-    plant_factor = float(items[0]['plant_factor'])   # the attribute requested
+    plant_factor = tables.table_query(
+        tables.PF_TABLE, 'plant_type', plant_type, 'plant_factor')
     print(plant_factor)  # for testing purposes
     return plant_factor
 
-def get_eto():
+def get_eto(date_time):
     # CIMIS API request
     # KEY: www.cimis.water.ca.gov
     print('requesting eto...')
     api_key = 'f972cb2a-79fb-440f-9e78-7bf034475e22'
     wsn_num = '204'  # WSN station #204 (SCV)
-    date = DATE_TIME.split(' ')[0]
+    start_end_date = date_time.split(' ')[0]
     payload = {
         'appKey': api_key,
         'targets': wsn_num,
-        'startDate': date,
-        'endDate': date,
+        'startDate': start_end_date,
+        'endDate': start_end_date,
         'dataItems': 'day-asce-eto'
         }
+
     eto = None
     while eto is None:
         try:
@@ -87,21 +66,5 @@ def get_eto():
                 full_response['Data']['Providers'][0]['Records'][0]['DayAsceEto']['Value'])
             print(eto)  # for testing purposes
         except TypeError:
-            print('get_eto failed')
+            print('...get_eto failed')
     return eto
-
-def write_results(pf, eto, water):
-    # put results to dynamoDB eve_sensor table
-    # pf, eto, water_alg
-    DATA_TABLE.update_item(
-        Key={
-            'plant_type': PLANT_TYPE,
-            'date_time': DATE_TIME
-        },
-        UpdateExpression='set pf = :pf, eto = :eto, water_alg= :alg',
-        ExpressionAttributeValues={
-            ':pf': pf,
-            ':eto': eto,
-            ':alg': water
-        }
-    )
