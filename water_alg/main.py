21d9553582306ece70dfa438925826289be5ed5d
porcupal <michael.d.sanchez@gmail.com>
2018-01-27 14:00:32 -0800

    jan 27 - corrected some runtime errors

--- water_alg/main.py
+++ water_alg/main.py
@@ -1,35 +1,34 @@
 import requests  # must be included in AWS deployment package
-import json
 import tables  # 'tables.py' script for querying and inserting to dynamoDB
 # PEP8 naming conventions for python variables and functions is snake_case
-# PEP8 convention for python comments is to start two spaces after a statement
+# PEP8 convention for python in-line comments is to start two spaces after a statement
+
 
 def water_alg(event, context):
     # water algorithm for determining watering amount
     # plot_area is omitted because it has not been implemented yet
-    user_info = tables.get_user_info() # list with attributes from 'eve_user' table
+    user = tables.get_user_info()  # list with attributes from 'eve_user' table
 
-    plant_type = user_info[1]
+    plant_type = user[1]
     date_time = str(
         event['Records'][0]['dynamodb']['Keys']['date_time']['S'])
 
     print('plant type = {}, date time = {}'.format(
         plant_type, date_time))  # for testing
 
-    # list with the values and results of 'water_alg' calculations
-    water_alg = [
-        get_pf(plant_type),
-        get_eto(date_time)
-        ]
+    pf = float(get_pf(plant_type))
+    eto = float(get_eto(date_time))
 
-    plot_size = user_info[3]
-    gal_water_reserve = eto * pf * 0.623 * plot_size
+    plot_size = float(user[3])
+    gal_water_reserve = pf * eto * 0.623 * plot_size
     cup_water_reserve = gal_water_reserve * 16  # converted to cups
     print ('water plot {} gallons or {} cups'.format(
         gal_water_reserve, cup_water_reserve))
 
-    water_alg.append(gal_water_reserve)
-    tables.write_results(user_info, water_alg)
+    # list with the values and results of 'water_alg' calculations
+    alg = [str(pf), str(eto), str(gal_water_reserve)]
+    tables.write_results(date_time, user, alg)
+
 
 def get_pf(plant_type):
     # query plantfactor from database
@@ -39,6 +38,7 @@ def get_pf(plant_type):
         tables.PF_TABLE, 'plant_type', plant_type, 'plant_factor')
     print(plant_factor)  # for testing purposes
     return plant_factor
+
 
 def get_eto(date_time):
     # CIMIS API request
@@ -62,8 +62,7 @@ def get_eto(date_time):
             full_response = req.json()
 
             # specifies the ETO from api request and assigns it to a variable
-            eto = float(
-                full_response['Data']['Providers'][0]['Records'][0]['DayAsceEto']['Value'])
+            eto = full_response['Data']['Providers'][0]['Records'][0]['DayAsceEto']['Value']
             print(eto)  # for testing purposes
         except TypeError:
             print('...get_eto failed')
--- water_alg/tables.py
+++ water_alg/tables.py
@@ -9,6 +9,7 @@ USER_TABLE = dynamodb.Table('eve_user')  # specifies the user table
 
 CURRENT_USER = 'zero'  # the value of your 'user_id' from 'USER_TABLE'
 
+
 # ---------- DYNAMODB TABLE FUNCTIONS ----------
 def get_user_info():
     # get information about the user, plant and plot size for water_alg
@@ -28,6 +29,7 @@ def get_user_info():
 
     return user_info
 
+
 def table_query(table, key, value, attr):
     # queries an attribute from dynamoDB table
     # table: the table being queried
@@ -35,12 +37,13 @@ def table_query(table, key, value, attr):
     # value: value of the primary key
     # attr: the attribute being queried
     response = table.query(
-        KeyConditionExpression = Key(key).eq(value))
+        KeyConditionExpression=Key(key).eq(value))
     items = response['Items']  # query returns a JSON object, 'Items'
 
     # assign the plant factor value from 'PF_TABLE' to a variable
     queried = items[0][attr]   # the attribute requested
     return queried
+
 
 def write_results(date_time, user, alg):
     # put results to dynamoDB eve_sensor table
